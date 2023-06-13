8b48a9cf4c3f7d30aee59026210bde1b8f5c04ae
quentinmathieu <quentin.mth@gmail.com>
2023-06-13 16:07:47 +0200

    fetch all commits with their codes changes

--- src/script.js
+++ src/script.js
@@ -2,45 +2,54 @@
 import {
   Octokit
 } from "https://cdn.skypack.dev/@octokit/core";
-      const octokit = new Octokit({ 
-auth: gitToken,
+const octokit = new Octokit({
+  auth: gitToken,
 });
 
 var result = null;
-
-
+var owner = "quentinmathieu"
+var repo = "Github_API"
 
 
 try {
   result = await octokit.request('GET /repos/{owner}/{repo}/commits', {
-    owner: 'quentinmathieu',
-    repo: 'SfEasyAdmin4',
-    headers: {
-      'X-GitHub-Api-Version': '2022-11-28'
-    }
+    owner: owner,
+    repo: repo,
+
   })
-
-  
-
-
 
 } catch (error) {
   console.log(`Error! Status: ${error.status}. Rate limit remaining: ${error.headers["x-ratelimit-remaining"]}. Message: ${error.response.data.message}`)
 }
-
-// result.data.forEach((commit) => {
-//     // console.log(commit.commit.message)
-//     console.log(commit.commit)
-// });
 const commitSHAs = result.data.map(commit => commit.sha);
-console.log(commitSHAs);
 
 
-// const result = await octokit.repos.compareCommits({
-//   owner: 'quentinmathieu',
-//   repo: 'SfEasyAdmin4',
-//   base: 'commit1_sha',
-//   head: 'commit2_sha'
-// });
 
-// console.log(result.data.files);
+
+
+
+
+
+
+async function getCommitChanges(commitSHAs) {
+  commitSHAs.forEach(async (commitSHA) => {
+    const commit = await octokit.request('GET /repos/{owner}/{repo}/commits/{commit_sha}', {
+      owner: owner,
+      repo: repo,
+      commit_sha: commitSHA
+    });
+
+    commit.data.files.forEach(file => {
+      if ((file.filename.slice(0, 6)) != "node_m" && file.patch)
+      {
+        console.log(`Changes to ${file.filename}:`);
+        console.log(file.patch);
+      }
+    });
+    // throw new Error("my error message");
+
+  });
+}
+
+getCommitChanges(commitSHAs);
+
